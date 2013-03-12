3b59214441e8be661fd8ed4c45bf76feabda6d51
khcr <kocher.ke@gmail.com>
2013-03-12 16:31:52 +0100

    inizialisation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neurones::Application.config.secret_token = '1ebba7a09129a0e9d5ea9635d697fd807ce4eb4171ff5a86474e2a9b99ef0b4abb38cc7c5cdeab4f64d97f6b3f13eea6bb8611eddce19ba7b3dfe7f94701504d'
