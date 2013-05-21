daa15c3cb3ba148464f6afe325c39f0503703e09
Noémien <nkcr.je@gmail.com>
2013-05-21 21:08:08 +0200

    design

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -7,32 +7,34 @@ class Admin::BaseController < ApplicationController
 
 	def connected?
 		element_id = Element.find_by_name(params[:controller]).id
+		@id_parents = Parent.where('user_id = ?', current_user).select('user_id')
+		@id_parents.push(current_user.id)
 		if !current_user.nil?
 			case params[:action]
 			when 'index'
-				@ownerships_all = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_read = ?', current_user.id, element_id, OwnershipType.find_by_name('all_entries').id, true )
-				@ownerships_on_ownership = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_read = ?', current_user.id, element_id, OwnershipType.find_by_name('on_ownership').id, true )
-				@ownerships_on_entry = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_read = ?', current_user.id, element_id, OwnershipType.find_by_name('on_entry').id, true ).select('id_element')
-				@update_ownerships = Ownership.where('user_id = ? AND element_id = ? AND right_update = ?', current_user.id, element_id, true)
-				@delete_ownerships = Ownership.where('user_id = ? AND element_id = ? AND right_delete = ?', current_user.id, element_id, true)
+				@ownerships_all = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_read = ?', @id_parents, element_id, OwnershipType.find_by_name('all_entries').id, true )
+				@ownerships_on_ownership = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_read = ?', @id_parents, element_id, OwnershipType.find_by_name('on_ownership').id, true )
+				@ownerships_on_entry = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_read = ?', @id_parents, element_id, OwnershipType.find_by_name('on_entry').id, true ).select('id_element')
+				@update_ownerships = Ownership.where('user_id IN (?) AND element_id = ? AND right_update = ?', @id_parents, element_id, true)
+				@delete_ownerships = Ownership.where('user_id IN (?) AND element_id = ? AND right_delete = ?', @id_parents, element_id, true)
 			when 'new' 
-				@ownerships_all = Ownership.where('user_id = ? AND element_id = ? AND right_create = ?', current_user.id, element_id, true )
+				@ownerships_all = Ownership.where('user_id IN (?) AND element_id = ? AND right_create = ?', @id_parents, element_id, true )
 				redirect_to root_path, notice: "Vous n'avez pas le droit d'ajouter un article." unless @ownerships_all.any?
 			when 'create'
-				@ownerships_all = Ownership.where('user_id = ? AND element_id = ? AND right_create = ?', current_user.id, element_id, true )
+				@ownerships_all = Ownership.where('user_id IN (?) AND element_id = ? AND right_create = ?', @id_parents, element_id, true )
 				redirect_to root_path, notice: "Vous n'avez pas le droit d'ajouter un article." unless @ownerships_all.any?
 			when 'edit'
-				@ownerships_all = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_update = ?', current_user.id, element_id, OwnershipType.find_by_name('all_entries').id, true )
-				@ownerships_on_ownership = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_update = ?', current_user.id, element_id, OwnershipType.find_by_name('on_ownership').id, true)
-				@ownerships_on_entry = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_update = ?', current_user.id, element_id, OwnershipType.find_by_name('on_entry').id, true).select('id_element')
+				@ownerships_all = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_update = ?', @id_parents, element_id, OwnershipType.find_by_name('all_entries').id, true )
+				@ownerships_on_ownership = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_update = ?', @id_parents, element_id, OwnershipType.find_by_name('on_ownership').id, true)
+				@ownerships_on_entry = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_update = ?', @id_parents, element_id, OwnershipType.find_by_name('on_entry').id, true).select('id_element')
 			when 'update'
-				@ownerships_all = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_update = ?', current_user.id, element_id, OwnershipType.find_by_name('all_entries').id, true )
-				@ownerships_on_ownership = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_update = ?', current_user.id, element_id, OwnershipType.find_by_name('on_ownership').id, true)
-				@ownerships_on_entry = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_update = ?', current_user.id, element_id, OwnershipType.find_by_name('on_entry').id, true).select('id_element')
+				@ownerships_all = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_update = ?', @id_parents, element_id, OwnershipType.find_by_name('all_entries').id, true )
+				@ownerships_on_ownership = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_update = ?', @id_parents, element_id, OwnershipType.find_by_name('on_ownership').id, true)
+				@ownerships_on_entry = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_update = ?', @id_parents, element_id, OwnershipType.find_by_name('on_entry').id, true).select('id_element')
 			when 'destroy'
-				@ownerships_all = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_delete = ?', current_user.id, element_id, OwnershipType.find_by_name('all_entries').id, true )
-				@ownerships_on_ownership = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_delete = ?', current_user.id, element_id, OwnershipType.find_by_name('on_ownership').id, true )
-				@ownerships_on_entry = Ownership.where('user_id = ? AND element_id = ? AND ownership_type_id = ? AND right_delete = ?', current_user.id, element_id, OwnershipType.find_by_name('on_entry').id, true ).select('id_element')
+				@ownerships_all = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_delete = ?', @id_parents, element_id, OwnershipType.find_by_name('all_entries').id, true )
+				@ownerships_on_ownership = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_delete = ?', @id_parents, element_id, OwnershipType.find_by_name('on_ownership').id, true )
+				@ownerships_on_entry = Ownership.where('user_id IN (?) AND element_id = ? AND ownership_type_id = ? AND right_delete = ?', @id_parents, element_id, OwnershipType.find_by_name('on_entry').id, true ).select('id_element')
 			end
 		else
 			redirect_to profil_path, notice: "Connectez-vous pour accéder à cette page."
