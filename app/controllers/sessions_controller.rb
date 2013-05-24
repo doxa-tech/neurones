a1b11efd89e530c39e2defa71f10a2975387f999
Noémien <nkcr.je@gmail.com>
2013-05-24 23:32:49 +0200

    auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,20 +27,23 @@ class SessionsController < ApplicationController
 		end
 	end
 
-	def create_facebook
+	def create_ext
 		raise env['omniauth.auth'].to_yaml
-		# raise env['omniauth.auth'].to_yaml
-		# params[:session][:email] = params[:session][:email].gsub(/\s+/, "").downcase
-		# user = User.find_by_email(params[:session][:email])
-		# if user && user.authenticate(params[:session][:password])
-		# 	params[:session][:remember_me] == '1' ? sign_in_permanent(user) : sign_in(user)
-		# 	respond_to do |format|
-  #     	format.html { redirect_to(root_path, notice: "Connexion réussie.") }
-  #     	format.js { render 'create_success' }
-  #   	end
-		# else
-		# 	redirect_to user_path action: 'create_F'
-		# end
+
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			params[:session][:remember_me] == '1' ? sign_in_permanent(user) : sign_in(user)
+			respond_to do |format|
+      	format.html { redirect_to(root_path, notice: "Connexion réussie.") }
+      	format.js { render 'create_success' }
+    	end
+		else
+			flash.now[:error] = "Nom d'utilisateur ou/et mot de passe incorrect."
+			respond_to do |format|
+      	format.html { render 'login' }
+      	format.js { render 'create_error' }
+    	end
+		end
 	end
 
 	def destroy 
