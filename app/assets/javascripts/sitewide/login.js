1905401d50df262b54cacc17331c9a14453dd837
Noémien <nkcr.je@gmail.com>
2013-06-01 00:40:43 +0200

    login, profil

--- app/assets/javascripts/sitewide/login.js
+++ app/assets/javascripts/sitewide/login.js
@@ -6,5 +6,15 @@ $(function() {
 		if(document.location.hash == "#log" && !$('#login .big-forms').is(':visible')) {
 			$.getScript('login' ) ;
 		}
-	})
+	});
+	$(window).scroll(function(){ 
+	  if ($(this).scrollTop() > 119){  
+	   // x should be from where you want this to happen from top//
+	    //make CSS changes here
+	    $('#login .login').css({position: 'relative', top: $(this).scrollTop()-120, right: '0'});
+	  } 
+	  else{
+	    $('#login .login').css({position: 'relative', top: 'inherit', right: 'inherit'});
+	  }
+	});
 });
