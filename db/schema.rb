624ab2dbdb6ff2e54d4d16e31e1e28066c201be3
Noémien <nkcr.je@gmail.com>
2013-06-09 14:10:46 +0200

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130526171045) do
+ActiveRecord::Schema.define(:version => 20130609120750) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
