ded34b461b709021aa19c3c2b70820d33b6ba9fc
Noémien <nkcr.je@gmail.com>
2013-06-09 23:11:10 +0200

    login fix

--- app/assets/javascripts/sitewide/login.js
+++ app/assets/javascripts/sitewide/login.js
@@ -8,12 +8,25 @@ $(function() {
 		}
 	});
 	// to keep login visible at top
-	$(window).scroll(function(){ 
-	  if ($(this).scrollTop() > 119){  
-	    $('#login .login').css({position: 'relative', top: $(this).scrollTop()-120, right: '0'});
-	  } 
-	  else{
-	    $('#login .login').css({position: 'relative', top: 'inherit', right: 'inherit'});
-	  }
+	$(window).scroll(function(){
+
+		if ($('#login .big-forms').is(':visible')) {
+
+			if ($(this).scrollTop() > (119+343)) {  
+		    $('#login .login').css({position: 'relative', top: $(this).scrollTop()-(120+344), right: '0'});
+		  } else {
+		    $('#login .login').css({position: 'relative', top: 'inherit', right: 'inherit'});
+		  }
+
+		} else {
+
+			if ($(this).scrollTop() > 119) {  
+		    $('#login .login').css({position: 'relative', top: $(this).scrollTop()-120, right: '0'});
+		  } else {
+		    $('#login .login').css({position: 'relative', top: 'inherit', right: 'inherit'});
+		  }
+
+		}
+
 	});
 });
