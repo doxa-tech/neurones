4e8f74a78e8666784563ffa6afb06eb698d652cf
khcr <kocher.ke@gmail.com>
2013-06-30 23:52:51 +0200

    fix mercury

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,7 +4,7 @@
 module ArticlesHelper
 
 	def image_src(article)
-		if article.mercury_image.nil?
+		if article.mercury_image_id.nil?
 			'/assets/blog/langoustine_1.jpg'
 		else
 			article.mercury_image.image.url
