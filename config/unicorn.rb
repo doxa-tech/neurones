43c526afb50a83dded3862633dc255e9d80115ba
khcr <kocher.ke@gmail.com>
2013-07-06 11:20:45 +0200

    deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,25 +11,19 @@ timeout 30
 preload_app true
 
 before_fork do |server, worker|
-  # Disconnect since the database connection will not carry over
-  if defined? ActiveRecord::Base
+  defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-  end
 
-  # Quit the old unicorn process
-  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+  old_pid = "#{server.config[:pid]}.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
     end
   end
 end
 
 after_fork do |server, worker|
-  # Start up the database connection again in the worker
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
+  ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.verify_active_connections!
 end
