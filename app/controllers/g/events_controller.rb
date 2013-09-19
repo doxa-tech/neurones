138cf077ca6514d5df59a1001915a1c029ee289a
khcr <kocher.ke@gmail.com>
2013-09-20 00:04:06 +0200

    fix

--- app/controllers/g/events_controller.rb
+++ app/controllers/g/events_controller.rb
@@ -5,7 +5,7 @@ class G::EventsController < G::BaseController
 	before_filter { |controller| controller.module_activated?('events')}
 
 	def show
-    unless @event = G::Event.find_by_group_id_and_id(current_group.try(:id), params[:id]) && module_activated?('events')} 
+    unless @event = G::Event.find_by_group_id_and_id(current_group.try(:id), params[:id])
       render 'public/404'
     end
   end
